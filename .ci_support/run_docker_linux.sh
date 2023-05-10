39247b1deec3e4ee1d5eeb3a83eb08ca36d6ac91
Julien Schueller <schueller@phimeca.com>
2023-05-10 15:44:07 +0200

    Sync to template

---
+++ .ci_support/run_docker_linux.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -xe
+
+cd /tmp
+mkdir build && cd build
+cmake -DCMAKE_INSTALL_PREFIX=~/.local \
+      -DCMAKE_UNITY_BUILD=OFF -DCMAKE_UNITY_BUILD_BATCH_SIZE=32 \
+      -DCMAKE_CXX_FLAGS="-Wall -Wextra -D_GLIBCXX_ASSERTIONS" \
+      -DSWIG_COMPILE_FLAGS="-O1 -Wno-unused-parameter" \
+      -DSPHINX_FLAGS="-W -T -j4" \
+      /io
+make install
+make tests
+ctest --output-on-failure --timeout 100 ${MAKEFLAGS}
+
+exit 0
+uid=$1
+gid=$2
+if test -n "${uid}" -a -n "${gid}"
+then
+  sudo cp -r ~/.local/share/*/doc/html /io
+  sudo chown -R ${uid}:${gid} /io/html
+fi
